58df76a8f57d9f5b03b37467015079a8c3721948
M-Tinati <tinatigithub@gmail.com>
2026-07-09 19:16:07 +0330

    Initial project-create base class

---
+++ GameEngine/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public short Health { get; set; }
+        public short Damage { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
