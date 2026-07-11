9ffcb26e386eecd851519a2b8d08f89084505cd2
M-Tinati <tinatigithub@gmail.com>
2026-07-11 17:00:00 +0330

    Control User Input and check Player List != null

--- GameTest/Menus/MianMenu.cs
+++ GameTest/Menus/MianMenu.cs
@@ -13,42 +13,71 @@ namespace GameTest.Menus
         {
             CreatePlayer = 1,
             InformationPlayer = 2,
-            Exit = 3
+            Enemy = 3,
+            Exit = 4
         }
 
         List<Player> players = new List<Player>();
 
         public void Start()
         {
+
             int Input = 0;
-            while (Input != 3)
+            while (Input != 4)
             {
-                Console.WriteLine("====================");
-                Console.WriteLine("========GAME========");
-                Console.WriteLine("====================");
-                Console.WriteLine($"1.{Startmenu.CreatePlayer}");
-                Console.WriteLine($"2.{Startmenu.InformationPlayer}");
-                Console.WriteLine($"3.{Startmenu.Exit}");
-                Console.WriteLine("Enter Row:");
-                Input = Convert.ToInt32(Console.ReadLine());
-                switch (Input)
+                try
                 {
-                    case 1:
-                        Console.WriteLine("Enter Name Character:");
-                        string NamePlayer = Console.ReadLine();
-                        players.Add(new Player(NamePlayer, 100, 1, 50));
-                        Console.WriteLine("Your character created");
-                        break;
-                    case 2:
-                        foreach (Player p in players)
-                        {
-                            Console.WriteLine($"Your name is:{p.Name} ,Health:{p.Health} ,Level:{p.Level} ,Gold:{p.Gold}");
-                        }
-                        break;
+                    Console.WriteLine("====================");
+                    Console.WriteLine("========GAME========");
+                    Console.WriteLine("====================");
+                    Console.WriteLine($"1.{Startmenu.CreatePlayer}");
+                    Console.WriteLine($"2.{Startmenu.InformationPlayer}");
+                    Console.WriteLine($"3.{Startmenu.Enemy}");
+                    Console.WriteLine($"4.{Startmenu.Exit}");
+                    Console.WriteLine("Enter Row:");
+                    Input = Convert.ToInt32(Console.ReadLine());
+                    switch (Input)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Name Character:");
+                            string NamePlayer = Console.ReadLine();
+                            players.Add(new Player(NamePlayer, 100, 1, 50));
+                            Console.WriteLine("Your character created");
+                            break;
+                        case 2:
+                            if (players.Count != 0)
+                            {
+                                foreach (Player p in players)
+                                {
+                                    Console.WriteLine($"Your name is:{p.Name} ,Health:{p.Health} ,Level:{p.Level} ,Gold:{p.Gold}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("not have player yet!!!!");
+                            }
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("please just number");
                 }
             }
             Console.WriteLine("exit");
         }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
 
