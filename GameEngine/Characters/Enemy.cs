ee7b7adebf5292b2319439f769284df83fb9c01e
M-Tinati <tinatigithub@gmail.com>
2026-07-11 17:07:19 +0330

    Enemy class

--- GameEngine/Characters/Enemy.cs
+++ GameEngine/Characters/Enemy.cs
@@ -4,8 +4,55 @@ using System.Text;
 
 namespace GameEngine.Characters
 {
-    public class Enemy : Character
+    public class Enemy
     {
-       
+        private string _name;
+        private int _health;
+        private int _damge;
+
+
+        /// <summary>
+        /// Health not should less than zero
+        /// zero == dead
+        /// </summary>
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Please Enter Your name !You can not empty this");
+                }
+                _name = value;
+            }
+        }
+
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Your Health value less than 0 you dead?");
+                }
+                _health = value;
+            }
+        }
+
+        public int Damage
+        {
+            get { return _damge; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Stop it You kill it");
+                }
+                _damge = value;
+            }
+        }
     }
 }
